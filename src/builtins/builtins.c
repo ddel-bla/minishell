8867c97b3923354864bdf401669b0cd98cf1dee7
fernandezclaudia <cfernandezg@bcds.net>
2024-06-20 22:26:57 +0200

    implementing env command

---
+++ src/builtins/builtins.c
@@ -0,0 +1,18 @@
+#include "../../include/minishell.h"
+
+/*
+ * Checks whether the executed command is one of the built-ins
+ * methods and executes it.
+ */
+void	exec_builtin(t_shell *shell)
+{
+	t_cmd	*cmd;
+
+	cmd = shell->cmd;
+	if (!ft_strcmp(cmd->cmd[0], "env"))
+		ft_env(shell);
+	else if (!ft_strcmp(cmd->cmd[0], "export"))
+		return ;//ft_export(shell);
+	else if (!ft_strcmp(cmd->cmd[0], "unset"))
+		return ;//ft_unset(shell);
+}
--- src/builtins/env/ft_env.c
+++ src/builtins/env/ft_env.c
@@ -1,21 +1,41 @@
 #include "../../../include/minishell.h"
 
+static int	count_arg(t_shell *shell);
+
 /*
  * Prints all the environment variables.
  */
-void	ft_env(t_env	*env)
+void	ft_env(t_shell	*shell)
 {
-	t_env	*aux;
+	t_cmd	*cmd;
+	t_env	*env;
 	char	*value;
 
-	aux = env;
-	while (aux)
+	cmd = shell->cmd;
+	env = shell->env;
+	if (count_arg(shell) > 1)
 	{
-		if (aux->value == NULL)
+		printf("env: <%s>: No such file or directory\n", cmd->cmd[1]);
+		shell->exit_status = 127;
+		return ;
+	}
+	while (env)
+	{
+		if (env->value == NULL)
 			value = "";
 		else
-			value = aux->value;
-		printf("%s=%s\n", aux->name, value);
-		aux = aux->next;
+			value = env->value;
+		printf("%s=%s\n", env->name, value);
+		env = env->next;
 	}
+}
+
+static int	count_arg(t_shell *shell)
+{
+	int	i;
+
+	i = 0;
+	while (shell->cmd->cmd[i])
+		i++;
+	return (i);
 }
